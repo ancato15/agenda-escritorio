910efc0d9e533754abc08bc45ac32406f7242ccd
ANDRES <ANDRES@PC>
2020-04-03 11:17:24 -0500

    Se realizo la poo con la clase cita y contacto

--- src/modelo/cita.java
+++ src/modelo/cita.java
@@ -1,14 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
-/**
- *
- * @author ANDRES
- */
+import java.util.Date;
+
+
+
 public class cita {
+    
+    private int idcita;
+    private int idcontacto;
+    private Date fecha;
+    private String hora;
+    private String lugar;
+    private String asunto;
+
+    public cita() {
+    }
+
+    public cita(int idcita, int idcontacto, Date fecha, String hora, String lugar, String asunto) {
+        this.idcita = idcita;
+        this.idcontacto = idcontacto;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.lugar = lugar;
+        this.asunto = asunto;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public int getIdcita() {
+        return idcita;
+    }
+
+    public void setIdcita(int idcita) {
+        this.idcita = idcita;
+    }
+
+    public int getIdcontacto() {
+        return idcontacto;
+    }
+
+    public void setIdcontacto(int idcontacto) {
+        this.idcontacto = idcontacto;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+    
+    
     
 }
--- src/modelo/contacto.java
+++ src/modelo/contacto.java
@@ -4,4 +4,73 @@ package modelo;
 
 public class contacto {
     
+    private int idcontacto;
+    private String nombre;
+    private String apellido;
+    private int celular;
+    private String correo;
+    private String direccion;
+
+    public contacto() {
+    }
+
+    public contacto(int idcontacto, String nombre, String apellido, int celular, String correo, String direccion) {
+        this.idcontacto = idcontacto;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.celular = celular;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getIdcontacto() {
+        return idcontacto;
+    }
+
+    public void setIdcontacto(int idcontacto) {
+        this.idcontacto = idcontacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+    
 }
